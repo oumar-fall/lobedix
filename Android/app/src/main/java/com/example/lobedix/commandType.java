5351f0958c633943c929629e4d2f4eeda27c238c
Fall <oumar.fall@telecom-paristech.fr>
2020-03-20 18:07:26 +0100

    creation CommandType

---
+++ Android/app/src/main/java/com/example/lobedix/commandType.java
@@ -0,0 +1,14 @@
+package com.example.lobedix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class commandType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command_type);
+    }
+}
