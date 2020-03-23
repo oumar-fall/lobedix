e217b957d7e55671668ee1c2688491ac0b051244
Marion Candelier <marion.candelier@telecom-paristech.fr>
2020-03-23 12:59:23 +0100

    valeurs nutritionnelles

---
+++ Android/app/src/main/java/com/example/lobedix/Dish_Informations_Nutri.java
@@ -0,0 +1,14 @@
+package com.example.lobedix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dish_Informations_Nutri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish__informations__nutri);
+    }
+}
