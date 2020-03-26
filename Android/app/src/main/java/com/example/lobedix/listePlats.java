d864c21534ec4bc54b565c1f28f6749092b26a51
Marion Candelier <marion.candelier@telecom-paristech.fr>
2020-03-26 16:46:32 +0100

    + ou -

--- Android/app/src/main/java/com/example/lobedix/listePlats.java
+++ Android/app/src/main/java/com/example/lobedix/listePlats.java
@@ -5,13 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class listePlats extends AppCompatActivity {
+
+    private int nbSpaghetti;
+    private int nbLasagnes;
+    private int nbCurry;
+    private int nbRatatouille;
+    private TextView lasagnes;
+    private TextView spaghetti;
+    private TextView curry;
+    private TextView ratatouille;
+
+    /*private  TextView spaghetti = findViewById(R.id.spaghetti);
+    private TextView ratatouille = findViewById(R.id.ratatouille);
+    private TextView curry = findViewById(R.id.ratatouille);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_plats);
+        lasagnes = findViewById(R.id.Textlasagnes);
+        spaghetti = findViewById(R.id.spaghetti);
+        ratatouille = findViewById(R.id.ratatouille);
+        curry = findViewById(R.id.curry);
+
     }
 
     public void infosLasagnes(View view) {
@@ -19,4 +39,54 @@ public class listePlats extends AppCompatActivity {
         Intent infosLasagnes = new Intent(listePlats.this, Dish_Informations.class);
         startActivity(infosLasagnes);
     }
+
+    public void addLasagnes(View view) {
+        nbLasagnes = nbLasagnes +1;
+        lasagnes.setText(Integer.toString(nbLasagnes));
+
+    }
+    public void removeLasagnes(View view){
+        if(nbLasagnes>0){
+            nbLasagnes = nbLasagnes -1;
+            lasagnes.setText(Integer.toString(nbLasagnes));
+        }
+    }
+
+    public void addSpaghetti(View view) {
+        nbSpaghetti = nbSpaghetti +1;
+        spaghetti.setText(Integer.toString(nbSpaghetti));
+
+    }
+    public void removeSpaghetti(View view){
+        if(nbSpaghetti>0){
+            nbSpaghetti = nbSpaghetti -1;
+            spaghetti.setText(Integer.toString(nbSpaghetti));
+        }
+    }
+
+    public void addCurry(View view) {
+        nbCurry = nbCurry +1;
+        curry.setText(Integer.toString(nbCurry));
+
+    }
+    public void removeCurry(View view){
+        if(nbCurry>0){
+            nbCurry = nbCurry -1;
+            curry.setText(Integer.toString(nbCurry));
+        }
+    }
+
+    public void addRatatouille(View view) {
+        nbRatatouille = nbRatatouille +1;
+        ratatouille.setText(Integer.toString(nbRatatouille));
+
+    }
+    public void removeRatatouille(View view){
+        if(nbRatatouille>0){
+            nbRatatouille = nbRatatouille -1;
+            ratatouille.setText(Integer.toString(nbRatatouille));
+        }
+    }
+
+
 }
