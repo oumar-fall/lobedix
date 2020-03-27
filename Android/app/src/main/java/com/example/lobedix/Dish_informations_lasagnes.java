6cd865ceb75afb1010e56231289a66ba37d35da4
Domitille <dommergues.domitille@gmail.com>
2020-03-27 09:09:23 +0100

    AJout fichiers manquants

---
+++ Android/app/src/main/java/com/example/lobedix/Dish_informations_lasagnes.java
@@ -0,0 +1,106 @@
+package com.example.lobedix;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Dish_informations_lasagnes extends AppCompatActivity {
+
+    ImageButton menu_button;
+    DrawerLayout drawer;
+    TextView avis;
+    ScrollView scrollview;
+    NavigationView view;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish__informations_lasagnes);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menu_button=(ImageButton)findViewById(R.id.menu_button);
+
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        avis = (TextView)findViewById(R.id.opinions);
+        scrollview = (ScrollView)findViewById(R.id.scroll);
+
+        avis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollview.scrollTo(0, avis.getBottom());
+            }
+        });
+
+        view=(NavigationView)findViewById(R.id.nav_view);
+
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+
+            @Override
+
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                CharSequence title = menuItem.getTitleCondensed();
+
+                if(title.equals("aperitif")){ // Choix apéro
+                        startActivity(new Intent(Dish_informations_lasagnes.this, ChoixApero.class));}
+
+                if (title.equals("espace")){ // Changer d'espace
+                        startActivity(new Intent(Dish_informations_lasagnes.this, commandType.class));}
+
+                if (title.equals("commande")){ // Ma commande
+                        startActivity(new Intent(Dish_informations_lasagnes.this, commandType.class));}
+
+                if (title.equals("entrees")){
+                    startActivity(new Intent(Dish_informations_lasagnes.this, listeEntrees.class));}
+
+                if (title.equals("plats")){ // Nos plats
+                        startActivity(new Intent(Dish_informations_lasagnes.this, listePlats.class));}
+
+                if (title.equals("desserts")){ // Nos desserts
+                        startActivity(new Intent(Dish_informations_lasagnes.this, commandType.class));}
+
+                if (title.equals("coupdepouce")){ // Coup de pouce
+                        startActivity(new Intent(Dish_informations_lasagnes.this, commandType.class));}
+
+                if (title.equals("quitter")){ // Quitter
+                        startActivity(new Intent(Dish_informations_lasagnes.this, MainActivity.class));}
+
+                drawer.closeDrawers();
+                return true;
+
+
+            }
+
+        });
+    }
+
+    public void nutriscore(View view){
+
+        Intent nutri = new Intent(Dish_informations_lasagnes.this, Dish_Informations_Nutri.class);
+        startActivity(nutri);
+    }
+
+
+
+
+}
---
+++ Android/app/src/main/java/com/example/lobedix/Dish_informations_taboule.java
@@ -0,0 +1,106 @@
+package com.example.lobedix;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Dish_informations_taboule extends AppCompatActivity {
+
+    private ImageButton menu_button;
+    private DrawerLayout drawer;
+    private TextView avis;
+    private ScrollView scrollview;
+    private NavigationView view;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish__informations_taboule);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menu_button=(ImageButton)findViewById(R.id.menu_button);
+
+
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        avis = (TextView)findViewById(R.id.opinions);
+        scrollview = (ScrollView)findViewById(R.id.scroll);
+
+        avis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollview.scrollTo(0, avis.getBottom());
+            }
+        });
+
+        view=(NavigationView)findViewById(R.id.nav_view);
+
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+
+            @Override
+
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                CharSequence title = menuItem.getTitleCondensed();
+
+                if(title.equals("aperitif")){ // Choix apéro
+                    startActivity(new Intent(Dish_informations_taboule.this, ChoixApero.class));}
+
+                if (title.equals("espace")){ // Changer d'espace
+                    startActivity(new Intent(Dish_informations_taboule.this, commandType.class));}
+
+                if (title.equals("commande")){ // Ma commande
+                    startActivity(new Intent(Dish_informations_taboule.this, commandType.class));}
+
+                if (title.equals("entrees")){
+                    startActivity(new Intent(Dish_informations_taboule.this, listeEntrees.class));}
+
+                if (title.equals("plats")){
+                    System.out.println("oueeeee");
+                    startActivity(new Intent(Dish_informations_taboule.this, listePlats.class));}
+
+                if (title.equals("desserts")){
+                    startActivity(new Intent(Dish_informations_taboule.this, commandType.class));}
+
+                if (title.equals("coupdepouce")){
+                    startActivity(new Intent(Dish_informations_taboule.this, commandType.class));}
+
+                if (title.equals("quitter")){
+                    startActivity(new Intent(Dish_informations_taboule.this, MainActivity.class));}
+
+                drawer.closeDrawers();
+                return true;
+
+
+            }
+
+        });
+    }
+
+    public void nutriscore(View view){
+
+        Intent nutri = new Intent(Dish_informations_taboule.this, Dish_Informations_Nutri.class);
+        startActivity(nutri);
+    }
+
+
+}
