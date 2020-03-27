013a580b42844275e498cafd98270464630f31dc
Marion Candelier <marion.candelier@telecom-paristech.fr>
2020-03-27 23:10:14 +0100

    je sais pas quoi dire

--- Android/app/src/main/java/com/example/lobedix/Espace_Enfant.java
+++ Android/app/src/main/java/com/example/lobedix/Espace_Enfant.java
@@ -3,6 +3,7 @@ package com.example.lobedix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Espace_Enfant extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class Espace_Enfant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_espace__enfant);
+        run();
+    }
+
+    public void run() {
+        TouchListener touchListener = new TouchListener();
+        ImageView lasagnes = findViewById(R.id.imageView);
+        lasagnes.setOnTouchListener(touchListener);
+
     }
 }
