78c9e24f9a29044f0e988df7ceba206441d32fce
domitille-dommergues <dommergues.domitille@gmail.com>
2020-03-29 22:30:35 +0200

    Fin des boissons

--- Android/app/src/main/java/com/example/lobedix/MyCommandeEnfant.java
+++ Android/app/src/main/java/com/example/lobedix/MyCommandeEnfant.java
@@ -1,8 +1,10 @@
 package com.example.lobedix;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -30,5 +33,33 @@ public class MyCommandeEnfant extends AppCompatActivity {
     public void retour(View view){
         Intent menus = new Intent(MyCommandeEnfant.this, Espace_Enfant2.class);
         startActivity(menus);
+    }
+
+    public void confirm(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Validation de la commande");
+        builder.setMessage("Etes-vous sûr.e de vouloir confirmer votre commande ?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Oui",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(MyCommandeEnfant.this, "Votre commande est en cours de préparation.", Toast.LENGTH_SHORT).show();
+                        DataHolder.getInstance().cleanListeDessert();
+                        DataHolder.getInstance().cleanListeEntree();
+                        DataHolder.getInstance().cleanListeMenu();
+                        DataHolder.getInstance().cleanListePlat();
+                        DataHolder.getInstance().cleanListeBoisson();
+                        startActivity(new Intent(MyCommandeEnfant.this, MainActivity.class));
+                    }
+                });
+
+        builder.setNegativeButton("Non",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
